a89bcda724c9b9b0b56bb48ad3026bfd1204203b
Gabriel Nascimento <166341117+uKuroo@users.noreply.github.com>
2025-10-01 18:52:06 -0300

    -- Colors

--- Animation.c
+++ Animation.c
@@ -3,12 +3,48 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+typedef struct Colors{
+    int id;
+    char colorCode[10];
+}Colors;
+
 int indexY = 0;
 int upOrDown = 0;
 
 void waitAndClear(){
     usleep(180000);
     system("clear");
+}
+
+void initializeColors(Colors colors[]){
+    colors[0].id = 0;
+    strcpy(colors[0].colorCode, ANSI_COLOR_RED);
+    
+    colors[1].id = 1;
+    strcpy(colors[1].colorCode, ANSI_COLOR_GREEN);
+
+    colors[2].id = 2;
+    strcpy(colors[2].colorCode, ANSI_COLOR_YELLOW);
+
+    colors[3].id = 3;
+    strcpy(colors[3].colorCode, ANSI_COLOR_BLUE);
+
+    colors[4].id = 4;
+    strcpy(colors[4].colorCode, ANSI_COLOR_MAGENTA);
+
+    colors[5].id = 5;
+    strcpy(colors[5].colorCode, ANSI_COLOR_CYAN);
+
+    colors[6].id = 6;
+    strcpy(colors[6].colorCode, ANSI_COLOR_RESET);
 }
 
 void animation(int indexSentence, char sentence[], int len, int qtdLines){
@@ -53,8 +89,33 @@ void startAnimation(char sentence[], int len, int qtdLines){
     }
 }
 
+int menuColors(){
+    printf("Choose one color\n");
+    printf("0 - RED\n");
+    printf("1 - GREEN\n");
+    printf("2 - YELLOW\n");
+    printf("3 - BLUE\n");
+    printf("4 - MAGENTA\n");
+    printf("5 - CYAN\n");
+    printf("6 - WHITE\n");
+
+    int colorNumber;
+    scanf("%d", &colorNumber);
+    system("clear");
+    return colorNumber;
+}
+
 int main()
 {
+    Colors colors[7];
+    initializeColors(colors);
+    
+    int colorNumber = menuColors();
+
+    getchar();
+    
+    printf("%s", colors[colorNumber].colorCode);
+    
     char sentence[100];
     int qtdLines = 0;
 
